55924830bef6ce8e655bd88e2da3426ccc1011ef
Anton Popov <popovanton567@gmail.com>
2025-05-26 20:15:31 +0200

    hw 2 question 4

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,13 @@
 import os
 import pickle
 import click
+import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+import mlflow
+import mlflow.sklearn
 
 
 def load_pickle(filename: str):
@@ -14,20 +18,23 @@ def load_pickle(filename: str):
 @click.command()
 @click.option(
     "--data_path",
-    default="./output",
+    default="../data_preprocessed/",
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
 
+    mlflow.sklearn.autolog()
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
+        print(f"RMSE: {rmse:.4f}")
 
 if __name__ == '__main__':
     run_train()
